3c320d1a76b0796e7551a4dff368a29bee2b1466
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-03-19 12:53:43 -0400

    Created functions for Add, CrossOut, and Delete

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//  Adding a new item to the list
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must enter something!");
+    } else {
+        $('#list').append(li);
+    }
+
+    // Crossing out an item on the list
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on("dblclick", crossOut);
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on("click", deleteListItem);
+
+    function deleteListItem() {
+        li.addClass("delete");
+    }
+
+    $('#list').sortable();
+}
+
+
+
+    
